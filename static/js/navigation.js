f01d11c6191df251da6d7b10d2227b9843823c5b
dm36 <dm36@rice.edu>
2015-01-31 23:59:36 -0600

    Event Handlin

--- static/js/navigation.js
+++ static/js/navigation.js
@@ -1,9 +1,34 @@
+//.class and # id
 $(document).ready(function(){
 	console.log('ready')
 
 	$('#create_new_group').click(function(){
 		console.log('create_new_group pressed')
-		$('.main_nav').css("left", "100%")
-		$('.new_group_form').css("left", "15%")
+		$('.new_group_form').css("left", "43%")
+		$('.new_group_form').css("top", "50%")
+		$('#create_new_group').css("display", "none")
+		$('#advanced_search').css("display", "none")
+		$('.quick_search_div').css("display", "none")
+	})
+
+	$('#advanced_search').click(function(){
+		console.log('advanced_search pressed')
+		$('.advanced_search_form').css("left", "43%")
+		$('.advanced_search_form').css("top", "50%")
+		$('#create_new_group').css("display", "none")
+		$('#advanced_search').css("display", "none")
+		$('.quick_search_div').css("display", "none")
+		$('.new_group_form').css("display", "none")
+		$('#form form').css("display", "none")
+	})
+
+	$('#new_group_form_submit').click(function(){
+		console.log('new_group_form pressed')
+		$('.text_box').css("left", "43%")
+		$('.text_box').css("top", "50%")
+		$('#create_new_group').css("display", "none")
+		$('#advanced_search').css("display", "none")
+		$('.quick_search_div').css("display", "none")
+
 	})
 })
